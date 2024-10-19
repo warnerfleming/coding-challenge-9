ee6d80fbefce8c482f0fd4012b90b5d5bfb87072
warnerfleming <fleming14@usf.edu>
2024-10-18 20:10:59 -0400

    Create Book class with properties and methods

--- library_management.js
+++ library_management.js
@@ -1,1 +1,24 @@
+// Task 1
 
+class Book {
+
+    constructor(title, author, ISBN,){
+        this.title = title
+        this.author = author
+        this.ISBN = ISBN 
+        this._isAvailable = true 
+    }
+
+    getDetails(title, author, ISBN,){
+        console.log(`${title}, ${author}, ${ISBN}`)
+    }
+
+    get isAvailable(){
+        if (this._isAvailable === true)
+            return this._isAvailable
+        else 
+        console.log(`Is borrowed`)
+}
+
+
+}
