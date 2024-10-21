00f83ca8ad004d187f5cae3e7cc900dc7790ec05
warnerfleming <fleming14@usf.edu>
2024-10-20 22:39:16 -0400

    Create Patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -27,6 +27,7 @@ class Book {
         }
     }
 
+    //Task 2
 
     class Section {
 
@@ -48,3 +49,29 @@ class Book {
         return this.books
     }
 }
+
+
+//Task 3
+
+class Patron{
+    constructor(name){
+    this.name
+    this.borrowedBooks = []
+}
+borrowBook(){
+    if (!book.isAvailable){
+        console.log(`Book is being borrowed at the moment`)
+     } else {
+        this.isAvailable = false
+    console.log(`Book is now being borrowed`)
+    }
+}
+returnBook(Book){
+    if (book.isAvailable) {
+        console.log(`Book is currently available`)
+    }
+    else {
+  book.isAvailable = true 
+  console.log (`Book has been returned`)
+
+}}}
